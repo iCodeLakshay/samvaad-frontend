bfca30a1176ba3a6142c1462bf3d46437c0bc3c6
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-12 16:39:04 +0530

    fix: add missing semicolon in App component export and update HashRouter import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router'
+
 
 createRoot(document.getElementById('root')).render(
     <HashRouter>
