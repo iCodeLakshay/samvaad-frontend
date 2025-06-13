760b58dd9c88428681d304fd7d7c97eaab351b81
iCodeLakshay <lakshaysaxena2217@gmail.com>
2025-06-13 12:22:18 +0530

    signup component fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
-import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
 import ProfilePage from './pages/ProfilePage';
 import Settings from './pages/Settings';
@@ -9,6 +8,7 @@ import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import { Toaster } from 'react-hot-toast';
 import { Navigate, Route, Routes } from 'react-router-dom';
+import SignupPage from './pages/SignUpPage';
 // import { Navigate, Route, Routes } from 'react-router';
 
 const App = () => {
@@ -33,7 +33,7 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path='/' element={ authUser ? <HomePage /> : <Navigate to='/login' />} />
-        <Route path='/signup' element={ !authUser ? <SignUpPage /> : <Navigate to='/' />} />
+        <Route path='/signup' element={ !authUser ? <SignupPage /> : <Navigate to='/' />} />
         <Route path='/login' element={ !authUser ? <LoginPage /> : <Navigate to='/' />} />
         <Route path='/settings' element={<Settings />} />
         <Route path='/profile' element={ authUser ? <ProfilePage /> : <Navigate to='/login' />} />
